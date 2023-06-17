0509659d07518f65e565613b99c2b6f11cedf3f3
bierooed <hov.hovanes13@gmail.com>
2023-06-17 04:45:59 +0400

    Wrote a fetch request to retrive random quotes and did some design work

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quoteInfo, setQuoteInfo] = useState({
+    quote: "",
+    author: "",
+  });
+  const [color, setColor] = useState("#27ae60");
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getRandomColor = () => {
+    const colors = [
+      "#16a085",
+      "#27ae60",
+      "#2c3e50",
+      "#f39c12",
+      "#e74c3c",
+      "#9b59b6",
+      "#FB6964",
+      "#342224",
+      "#472E32",
+      "#BDBB99",
+      "#77B1A9",
+      "#73A857",
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+
+  const changeColor = () => {
+    const newColor = getRandomColor();
+    setColor(newColor);
+    document.body.style.backgroundColor = newColor;
+  };
+
+  const getQuote = async () => {
+    const options = {
+      method: "POST",
+      headers: {
+        "X-RapidAPI-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      },
+    };
+    const response = await fetch(
+      "https://andruxnet-random-famous-quotes.p.rapidapi.com",
+      options
+    );
+    const data = await response.json();
+    setQuoteInfo({ quote: data[0].quote, author: data[0].author });
+    changeColor();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="quote-box">
+        <div id="quote-text">
+          <h5>{quoteInfo.quote}</h5>
+        </div>
+        <div id="quote-author">
+          <h5 style={{ textAlign: "end" }}> - {quoteInfo.author}</h5>
+        </div>
+        <div id="buttons">
+          {!!quoteInfo.quote && (
+            <button style={{ backgroundColor: color }} onClick={getQuote}>
+              Get Quote
+            </button>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
